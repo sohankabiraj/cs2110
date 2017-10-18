075be80ac8af69abd61135d1c0024ea981c37cdd
Sohan Kabiraj <32105731+sohankabiraj@users.noreply.github.com>
2017-10-18 14:02:26 -0400

    Playable Interface

--- HW4/Playable.java
+++ HW4/Playable.java
@@ -1,1 +1,17 @@
+/**
+ * Sohan Kabiraj (sk5gb) 
+ **/
 
+
+public interface Playable {
+/**
+ * interface class with all the methods that the Song and the PlayList class will implement 	
+ */
+	
+	public void play();
+	public String getName();
+	public int getPlayTimeSeconds();
+	public int numberOfSongs();
+	
+	
+}
